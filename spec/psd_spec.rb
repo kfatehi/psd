ac6caf081d2bf80233263aaf5f8c5792ed00167b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2012-10-21 18:35:34 -0700

    really

--- spec/psd_spec.rb
+++ spec/psd_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Psd do
   it "can read psd file headers" do
     psdfile = Psd::PsdFile.new(TEST_FILE_PATH)
-    binding.pry
+    psdfile.parse.headers.should_not eq nil
   end
 end
